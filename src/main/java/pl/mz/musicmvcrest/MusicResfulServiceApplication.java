9339275f73bf0df3df57affd001f9763fbce617f
unknown <marcin.it.zietek@gmail.com>
2022-12-27 19:47:30 +0100

    first commit

---
+++ src/main/java/pl/mz/musicmvcrest/MusicResfulServiceApplication.java
@@ -0,0 +1,13 @@
+package pl.mz.musicmvcrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicResfulServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicResfulServiceApplication.class, args);
+	}
+
+}
